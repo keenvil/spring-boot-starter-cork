08ed479dbdd7b0e1212a6d6000652ee40d7eec8a
marioroman <marioroman@gmail.com>
2016-06-09 16:38:52 -0300

    AC-70 Split-common-modules
    * Separate common artifacts to a new repository.
    * security, common classes for security,
    * error, common clases for API error responses.

---
+++ commons-core/src/test/java/io/mycommunity/commons/domain/ResponseErrorBuilderTest.java
@@ -0,0 +1,43 @@
+package io.mycommunity.commons.domain;
+
+import static org.easymock.EasyMock.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.easymock.EasyMockRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+
+import io.mycommunity.commons.component.ErrorTranslator;
+import io.mycommunity.commons.error.BaseError;
+
+@RunWith(EasyMockRunner.class)
+public class ResponseErrorBuilderTest {
+
+  private ErrorTranslator parser = new ErrorTranslator();
+
+  private BindingResult result;
+
+  @Test
+  public void parse() {
+    result = createMock(BindingResult.class);
+    ObjectError error =
+        new ObjectError("Visitor", new String[]{"firstName"}, null,
+            "Default message");
+    List<ObjectError> objectErrors = new ArrayList<>();
+    objectErrors.add(error);
+    expect(result.getAllErrors()).andReturn(objectErrors);
+    replay(result);
+
+    List<BaseError> errors = parser.translate(result);
+    assertThat(errors, notNullValue());
+    assertThat(errors.get(0).getCode(), is("firstName"));
+    assertThat(errors.get(0).getMessage(), is("Default message"));
+    verify(result);
+  }
+}
