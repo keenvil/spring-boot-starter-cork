8f825e6e8c7e89378a58d593613584ac2cb0f5cd
marioroman <mario.roman@gmail.com>
2016-06-15 23:22:58 -0300

    Ac 70 split common modules (#2)
    * AC-70 Split-common-modules
    
    * Separate common artifacts to a new repository.
    * security, common classes for security,
    * error, common clases for API error responses.
    
    * * Refactor packages names.
    
    * * Refactor packages names.
    
    * * README information.
    
    * * New Platform exception.

---
+++ commons-core/src/main/java/io/mycommunity/commons/error/ErrorTranslator.java
@@ -0,0 +1,36 @@
+package io.mycommunity.commons.error;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.Validate;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+
+/** Translates Spring binding results into platform error codes.
+ * <p>Platform codes are as following:
+ * <ul>
+ * <li> Code as error.rejectedEntity.rejectedProperty (Size.userAccount.name)
+ * <li> Default Message (username length must be between 5 and 255 characters)
+ * </ul>
+ */
+@Component
+public class ErrorTranslator {
+
+  /** Translate Spring binding results into platform specific error codes. 
+   * @param result binding errors
+   * @return list of platform specific errors
+   */
+  public List<BaseError> translate(final BindingResult result) {
+    
+    Validate.notNull(result);
+    List<BaseError> fieldErrors = new ArrayList<BaseError>();
+    List<ObjectError> allErrors = result.getAllErrors();
+    for (ObjectError objectError : allErrors) {
+      fieldErrors.add(new BaseError(objectError.getCodes()[0],
+          objectError.getDefaultMessage()));
+    }
+    return fieldErrors;
+  }
+}
