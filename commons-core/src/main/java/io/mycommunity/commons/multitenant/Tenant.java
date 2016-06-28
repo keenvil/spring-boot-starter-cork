8c929bf3a488d8f10cb86c919384b88aa56337f1
marioroman <marioroman@gmail.com>
2016-06-28 16:41:20 -0300

    C-98 multitenancy-support
    * Adds Multi Tenant/Community support to the platform,
    * Theres three main parts for this: a filter which catchs a http header
    with the community id, a Identifier resolver which returns the selected
    community id, and a datasource resolver which transform that id to
    datasource to be uses by the module,
    * Changes versions numbers to SEM model,
    * Changes configuration approaches to Base Security object to prevent
    circular dependencies.

---
+++ commons-core/src/main/java/io/mycommunity/commons/multitenant/Tenant.java
@@ -0,0 +1,81 @@
+package io.mycommunity.commons.multitenant;
+
+import org.apache.commons.lang3.Validate;
+
+/** Represents a tenant/community in the platform.
+ * 
+ * <p>Each Tenant has its own datasource to a different database schema.</p>
+ */
+public class Tenant {
+
+  private String name;
+
+  private boolean isDefault;
+
+  private String url;
+
+  private String username;
+
+  private String password;
+
+  private String driverClassName;
+
+  public Tenant() { }
+
+  public String getName() {
+    return name;
+  }
+
+  public boolean isDefaultTenant() {
+    return isDefault;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getDriverClassName() {
+    return driverClassName;
+  }
+
+  public boolean isDefault() {
+    return isDefault;
+  }
+
+  public void setDefault(boolean isDefault) {
+    this.isDefault = isDefault;
+  }
+
+  public void setName(String aName) {
+    Validate.notBlank(aName, "Name cannot be empty.");
+    name = aName;
+  }
+
+  public void setUrl(String aUrl) {
+    Validate.notBlank(aUrl, "Url cannot be empty.");
+    url = aUrl;
+  }
+
+  public void setUsername(String aUsername) {
+    Validate.notBlank(aUsername, "Username cannot be empty.");
+    username = aUsername;
+  }
+
+  public void setPassword(String aPassword) {
+    Validate.notBlank(aPassword, "Password cannot be empty.");
+    password = aPassword;
+  }
+
+  public void setDriverClassName(String aDriverClassName) {
+    Validate.notBlank(aDriverClassName, "Driver cannot be empty.");
+    driverClassName = aDriverClassName;
+  }
+}
