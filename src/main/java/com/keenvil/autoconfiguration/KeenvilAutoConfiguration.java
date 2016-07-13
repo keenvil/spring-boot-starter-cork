39d7819e9a4df97add01f1612371fd0c5a4dec7d
Franco <franco.monsalvo@gmail.com>
2016-07-13 14:37:28 -0300

    removes mail service from commons

--- src/main/java/com/keenvil/autoconfiguration/KeenvilAutoConfiguration.java
+++ src/main/java/com/keenvil/autoconfiguration/KeenvilAutoConfiguration.java
@@ -1,74 +1,19 @@
 package com.keenvil.autoconfiguration;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClient;
-
-import com.keenvil.mail.service.EmailService;
 import com.keenvil.web.security.jwt.JwtService;
 import com.keenvil.web.security.service.PlatformSecurityService;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.cloud.aws.mail.simplemail.SimpleEmailServiceJavaMailSender;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
-import org.springframework.mail.javamail.JavaMailSender;
-
 @Configuration
 public class KeenvilAutoConfiguration {
-
-  @Value("${access_key}")
-  private String accessKey;
-
-  @Value("${secret_key}")
-  private String secretKey;
-
-  @Value("${cloud.aws.region}")
-  private String region;
 
   @Bean
   public static PropertySourcesPlaceholderConfigurer propertyConfigIn() {
     return new PropertySourcesPlaceholderConfigurer();
-  }
-
-  /** Basic AWS Credentials.
-   * @return AWS bean credentials.
-   */
-  @Bean
-  public AWSCredentials basicAWSCredentials() {
-    return new BasicAWSCredentials(accessKey, secretKey);
-  }
-
-  /** Simple email service.
-   * @param credentials credentials.
-   * @return email service.
-   */
-  @Bean
-  public AmazonSimpleEmailService amazonSimpleEmailService(
-      AWSCredentials credentials) {
-    AmazonSimpleEmailService amazonSimpleEmailService = 
-        new AmazonSimpleEmailServiceClient(credentials);
-    amazonSimpleEmailService.setRegion(Region.getRegion(Regions
-        .valueOf(region)));
-    return amazonSimpleEmailService;
-  }
-
-  @Bean
-  public JavaMailSender mailSender(
-      AmazonSimpleEmailService amazonSimpleEmailService) {
-    return new SimpleEmailServiceJavaMailSender(amazonSimpleEmailService);
-  }
-
-  @Bean
-  @ConditionalOnMissingBean
-  public EmailService emailService() {
-    return new EmailService();
   }
 
   @Bean(name = "securityService")
