f6ebfb4e58380708400a374db8674c66721a2c0c
marioroman <marioroman@gmail.com>
2016-08-19 17:47:13 -0300

    AC-215 add-interest-to-resident
        * Base clases for localization support,
          - Base entity class for Localization,
          - Service annotation to make it localizable,
          - Advice to enable and set localizable filter.

---
+++ src/main/java/com/keenvil/internationalization/LocalizableAspect.java
@@ -0,0 +1,51 @@
+package com.keenvil.internationalization;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.hibernate.Session;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Aspect to make Rest Service call localizable. Please refere to
+ * {@link Localizable} for further information.
+ */
+@Aspect
+@Component
+public class LocalizableAspect {
+
+  private static Logger log =  getLogger(LocalizableAspect.class);
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  /** Aspect to localize a service call.
+   * <p>This aspects enables the {@link Localizable#FILTER_NAME}</p> and
+   * sets its value with the first argument of the advised call.
+   * @param pointcut point cut.
+   * @param localized localized annotation.
+   */
+  @Before("@annotation(localized)")
+  public void localize(JoinPoint pointcut, Localized localized) {
+    String locale = (String) pointcut.getArgs()[0];
+
+    if (log.isDebugEnabled()) {
+      log.debug("Localizing service call {} with {}",
+          pointcut.toShortString(), locale);
+    }
+    getSession()
+      .enableFilter(Localizable.FILTER_NAME)
+      .setParameter(Localizable.FILTER_PARAM, locale);
+  }
+
+  private Session getSession() {
+    Session session = entityManager.unwrap(Session.class);
+    return session;
+  }
+}
