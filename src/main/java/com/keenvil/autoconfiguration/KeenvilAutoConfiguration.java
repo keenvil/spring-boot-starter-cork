acc1d1c460383daf64198fdde72f6b2e7ef0eab1
marioroman <marioroman@gmail.com>
2016-09-08 13:56:15 -0300

    community-integration
        * Adds an static way to access Jwt token and Community to be forwarded
          by Feign clients in subsequent API calls.
        * Data is sotred in the ThreadLocal variables.

--- src/main/java/com/keenvil/autoconfiguration/KeenvilAutoConfiguration.java
+++ src/main/java/com/keenvil/autoconfiguration/KeenvilAutoConfiguration.java
@@ -1,14 +1,20 @@
 package com.keenvil.autoconfiguration;
 
+import com.keenvil.core.multitenant.CommunityResolverFilter;
 import com.keenvil.internationalization.LocalizableAspect;
 import com.keenvil.web.security.jwt.JwtAuthenticationEntryPoint;
 import com.keenvil.web.security.jwt.JwtService;
+import com.keenvil.web.security.jwt.JwtTokenHolder;
 import com.keenvil.web.security.service.PlatformSecurityService;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 public class KeenvilAutoConfiguration {
@@ -34,6 +40,25 @@ public class KeenvilAutoConfiguration {
   @ConditionalOnMissingBean
   public JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint() {
     return new JwtAuthenticationEntryPoint();
+  }
+
+  /**
+   * Feign Request Intercepter in charge of forwarding Jwt Authentication
+   * Token and Community to other services calls fired by the main call.
+   */
+  @Bean
+  @ConditionalOnMissingBean
+  public RequestInterceptor requestInterceptor() {
+    return new RequestInterceptor() {
+      
+      @Override
+      public void apply(RequestTemplate template) {
+        SecurityContextHolder.getContext().getAuthentication();
+        template.header(JwtService.X_AUTHORIZATION, JwtTokenHolder.token());
+        template.header(CommunityResolverFilter.X_COMMUNITY_ID,
+            JwtTokenHolder.community());
+      }
+    };
   }
 
   @Bean
