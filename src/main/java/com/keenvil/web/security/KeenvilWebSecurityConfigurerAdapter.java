3d078aca9d999beec6b7fc2316258866e2f7257d
Mario Roman <mario@keenvil.com>
2017-03-28 15:27:27 -0300

    allow-submodules-to-exclude-endpoints     * Allow modules to exclude endpoints from security check.

--- src/main/java/com/keenvil/web/security/KeenvilWebSecurityConfigurerAdapter.java
+++ src/main/java/com/keenvil/web/security/KeenvilWebSecurityConfigurerAdapter.java
@@ -1,14 +1,17 @@
 package com.keenvil.web.security;
 
-import com.keenvil.web.security.jwt.JwtAuthenticationEntryPoint;
-import com.keenvil.web.security.jwt.JwtAuthenticationFilter;
-import com.keenvil.web.security.jwt.JwtService;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.keenvil.web.security.jwt.JwtAuthenticationEntryPoint;
+import com.keenvil.web.security.jwt.JwtAuthenticationFilter;
+import com.keenvil.web.security.jwt.JwtService;
 
 public class KeenvilWebSecurityConfigurerAdapter
     extends WebSecurityConfigurerAdapter {
@@ -21,25 +24,33 @@ public class KeenvilWebSecurityConfigurerAdapter
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    List<String> endpoints = new ArrayList<String>();
+    endpoints.add("/");
+    endpoints.add("/account/auth");
+    endpoints.add("/account/validate");
+    endpoints.add("/configuration/**");
+    endpoints.add("/health");
+    endpoints.add("/info");
+    endpoints.add("/status");
+    
+    String notSecurized = getNotSecurizedEndpoint();
+    if (notSecurized != null && !notSecurized.isEmpty()) {
+      endpoints.add(notSecurized);
+    }
+
     http.csrf().disable()
         .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
         .and()
         .sessionManagement()
-          .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
         .authorizeRequests()
-        .antMatchers("/",
-          "/account/auth",
-          "/account/validate",
-          "/configuration/**",
-          "/health",
-          "/info",
-          "/status").permitAll()
-          .anyRequest()
-          .authenticated()
-          .and()
-          .addFilterBefore(
-          authenticationTokenFilter(),
+        .antMatchers(endpoints.toArray(new String[]{}))
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .addFilterBefore(authenticationTokenFilter(),
           UsernamePasswordAuthenticationFilter.class);
   }
 
@@ -49,5 +60,14 @@ public class KeenvilWebSecurityConfigurerAdapter
         new JwtAuthenticationFilter(jwtService);
     filter.setAuthenticationManager(authenticationManagerBean());
     return filter;
+  }
+
+  /**
+   * Gets the not securized end points for a securized module.
+   * 
+   * @return securized end points as AntMatchers.
+   */
+  public String getNotSecurizedEndpoint() {
+    return "";
   }
 }
