181091fc84dcfe0c7feb0412043ffac491adeb73
marioroman <mario.roman@gmail.com>
2017-03-30 08:30:52 -0700

    Merge master into dev. (#36)

---
+++ commons-core/src/main/java/io/mycommunity/commons/error/BaseError.java
@@ -0,0 +1,35 @@
+package io.mycommunity.commons.error;
+
+import org.apache.commons.lang3.Validate;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/** Presents any kind of business or validation error that should be informed
+ * throw an API service.
+ */
+@ApiModel
+public class BaseError {
+
+  @ApiModelProperty(required = true, value = "Error Code")
+  private String code;
+
+  @ApiModelProperty(required = false, value = "Error description.")
+  private String message;
+
+  public BaseError() { }
+
+  public BaseError(final String aCode, final String aMessage) {
+    Validate.notEmpty(aCode);
+    code = aCode;
+    message = aMessage;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
---
+++ commons-core/src/main/java/io/mycommunity/commons/error/PlatformException.java
@@ -0,0 +1,57 @@
+package io.mycommunity.commons.error;
+
+import java.util.List;
+
+/** Platform common errors.
+ * <p>Platform common errors are application common errors than can be reused
+ * across the platform.
+ */
+@SuppressWarnings("serial")
+public class PlatformException extends RuntimeException {
+
+  public PlatformException() { }
+
+  public PlatformException(final String message) {
+    super(message);
+  }
+
+  /** Used to indicate that a user already exists into the platform.
+   */
+  public static class UserAlreadyExists extends PlatformException {
+
+    public UserAlreadyExists() { }
+
+    public UserAlreadyExists(final String message) {
+      super(message);
+    }
+  }
+
+  /** Used to indicate that there was some validation errors in a API service.
+   */
+  public static class ValidationError extends PlatformException {
+
+    public List<BaseError> validationErrors;
+
+    public ValidationError() { }
+
+    public ValidationError(final List<BaseError> someErrors) {
+      validationErrors = someErrors;
+    }
+
+    public List<BaseError> getValidationErrors() {
+      return validationErrors;
+    }
+  }
+
+  /** Used to indicate that there was a problem trying to authenticate a user
+   * into the platform.
+   */
+  public static class Authorization extends PlatformException {
+
+    public Authorization() { }
+
+    public Authorization(final String message) {
+      super(message);
+    }
+  }
+}
